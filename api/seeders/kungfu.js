350d4590837b7bf7aa9089871b686c1665a65f6e
Khanh <tran_khanh@outlook.com>
2020-02-07 11:33:47 +0700

    implement migrate data mock

---
+++ api/seeders/kungfu.js
@@ -0,0 +1,51 @@
+'user strict';
+
+import { Sequelize } from "sequelize/types";
+
+module.export = {
+    up: (queryInterface, Sequelize) => {
+
+        return queryInterface.builkInsert('kungus', [
+            {
+                name: 'Clothes for Men',
+                description: 'A monthly supply of trendy clothes for men.',
+                createdAt: new Date(),
+                updatedAt: new Date()
+            },
+            {
+                name: 'Clothes for Women',
+                description: 'A monthly supply of trendy clothes for women.',
+                createdAt: new Date(),
+                updatedAt: new Date()
+            },
+            {
+                name: 'Accessories for Men',
+                description: 'A monthly supply of trendy accessories for men',
+                createdAt: new Date(),
+                updatedAt: new Date()
+            },
+            {
+                name: 'Accessories for Women',
+                description: 'A monthly supply of trendy accessories for women',
+                createdAt: new Date(),
+                updatedAt: new Date()
+            },
+            {
+                name: 'Clothes and Accessories for Men',
+                description: 'A monthly supply of trendy clothes and accessories for men',
+                createdAt: new Date(),
+                updatedAt: new Date()
+            },
+            {
+                name: 'Clothes and Accessories for Women',
+                description: 'A monthly supply of trendy clothes and accessories for women',
+                createdAt: new Date(),
+                updatedAt: new Date()
+            }
+        ], {})
+
+    },
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.bulkDelete('crates', null, {});
+    }
+}
