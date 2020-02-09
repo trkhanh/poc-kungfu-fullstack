7ed5eb6160a5924a5c33d28c1de4ac8356a7138f
Khanh <tran_khanh@outlook.com>
2020-02-09 23:08:04 +0700

    update button and common and color component

---
+++ mobile/src/ui/button/Button.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {StyleSheet, View, Text} from 'react-native'
+
+import { buttonPadding, blockMarginHalf, buttonRadius, buttonHeight, font } from '../common/responsive'
+import { primary, secondary, white, grey2, grey4 } from '../common/colors'
+import Icon from '../icon/Icon'
+import Touchable from '../touchable/Touchable'
